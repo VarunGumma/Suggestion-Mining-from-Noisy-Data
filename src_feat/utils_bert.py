8a93494773fb7a70b86369883838d3d21413131e
Varun Gumma <varun230999@gmail.com>
2022-07-17 16:48:37 +0800

    experimenting with pos-tags and LSTM models

--- src_feat/utils_bert.py
+++ src_feat/utils_bert.py
@@ -1,29 +1,7 @@
 from transformers import RobertaTokenizerFast, BertTokenizerFast
 import pandas as pd
 
-# def _extend_labels(L, text, labels, tokenizer):
-#     X = 5
-#     extended_labels = [X]
-#     for (t, l) in zip(text, labels):
-#         n = len(tokenizer.tokenize(t))
-#         extended_labels.extend([l] * n)
-#     rem = L - len(extended_labels)
-#     extended_labels.extend([X] * rem)
-#     return extended_labels
-
-
-# def _extend_labels(L, text, labels, tokenizer, tag2idx):
-#     extended_labels = [tag2idx['<']]
-#     for (t, l) in zip(text, labels):
-#         n = len(tokenizer.tokenize(t))
-#         extended_labels.extend([l] * n)
-#     extended_labels.append(tag2idx['>'])
-#     rem = L - len(extended_labels)
-#     extended_labels.extend([tag2idx['$']] * rem)
-#     return extended_labels
-
-# '<' - START, '>' - END, '$' - PAD
-def get_encoded_input(fname, tag2idx, tokenizer_name="roberta-base"):
+def get_encoded_input(fname, tag2idx=None, tokenizer_name="roberta-base"):
 
     def _extend_labels(txt, lbls):
         extended_labels = [tag2idx['<']]
@@ -48,14 +26,13 @@ def get_encoded_input(fname, tag2idx, tokenizer_name="roberta-base"):
                        converters={'a': pd.eval, 
                                    'b': pd.eval})
 
-    txt = data['a'].tolist()
     labels = [[tag2idx[l.split('-')[0]] for l in labels] for labels in data['b']]
 
-    ext_txt = tokenizer(txt, 
-                         padding="longest",
-                         return_attention_mask=True,
-                         is_split_into_words=True,
-                         return_length=True)
+    ext_txt = tokenizer(data['a'].tolist(), 
+                        padding="longest",
+                        return_attention_mask=True,
+                        is_split_into_words=True,
+                        return_length=True)
 
     max_len = ext_text["length"][0]
     ext_labels = [_extend_labels(txt, lbl) for (txt, lbl) in zip(text, labels)]
